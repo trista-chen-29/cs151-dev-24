4a798332fbdee8160a7733c0210f27b13dce8d95
Gjaime650 <gjaime124@gmail.com>
2025-11-15 13:31:19 -0800

    Added commentsService methods

--- src/main/java/cs151/application/studentprofile/CommentsService.java
+++ src/main/java/cs151/application/studentprofile/CommentsService.java
@@ -1,4 +1,17 @@
 package cs151.application.studentprofile;
 
+import cs151.application.persistence.CommentDAO;
+
+import java.util.List;
+
 public class CommentsService {
+    CommentDAO commentDAO = new CommentDAO();
+
+    public List<Comment> listComments(long id){
+       return commentDAO.listByStudent(id);
+    }
+    public boolean addComment(long id, String comment){
+        return commentDAO.add(id, comment) != -1;
+    }
+
 }
